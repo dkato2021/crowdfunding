b68d3b273805254de2f21bbcf4c71126b2062dff
buncho <78598272+buncho1021@users.noreply.github.com>
2021-02-10 11:28:48 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 cd run
 
+echo "Preprocessing"
 python3 1.preprocess1.py
 
 python3 1.preprocess2.py
 
 python3 1.preprocess3.py
 
+echo "Learning"
 python3 2.learning.py
 
+echo "inferring"
 python3 3.inference.py
